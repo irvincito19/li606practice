0a5b32c034da2e8f4721d8a16899586cf4695bf6
Irving Hernández - 606 <irving.u.h.m@gmail.com>
2025-04-11 18:56:14 -0600

    Añadido el autor del programa y su email

--- hola.php
+++ hola.php
@@ -1,3 +1,5 @@
-<?php // El nombre por defecto es Mundo 
+<?php 
+//Autor: Irving Hernández
+// El nombre por defecto es Mundo 
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
