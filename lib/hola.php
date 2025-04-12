2ec689823636721869ab27589d639a2af0b9bd1b
Irving Hernández - 606 <irving.u.h.m@gmail.com>
2025-04-11 19:14:56 -0600

    Movido hola.php a lib

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,5 @@
 <?php 
-//Autor: Irving Hernández
+//Autor: Irving Hernández <irving@unsis.edu.mx>
 // El nombre por defecto es Mundo 
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
