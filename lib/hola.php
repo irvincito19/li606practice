c64c336ba1966ededa9cbd677475cc148d827d28
Irving Hernández - 606 <irving.u.h.m@gmail.com>
2025-04-20 22:53:30 -0600

    hola usa la clase HolaMundo

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,7 @@
 <?php 
 //Autor: Irving Hernández <irving@unsis.edu.mx>
 // El nombre por defecto es Mundo 
+require('HolaMundo.php');
+ 
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
